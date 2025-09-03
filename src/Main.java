3dc0c88636138f6184514214d1d83eb35138310b
Joshua Bugeja <joshua.bugeja@students.mq.edu.au>
2025-09-03 16:29:29 +1000

    Finished Task 13 and 14

--- src/Main.java
+++ src/Main.java
@@ -19,6 +19,8 @@ public class Main extends JFrame {
           stage = StageReader.readStage("data/stage1.rvb");
         } catch (IOException e) {
           e.printStackTrace();
+          System.err.println("Failed to read file");
+          System.exit(2);
         }
       }
 
--- src/StageReader.java
+++ src/StageReader.java
@@ -1,12 +1,35 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class StageReader {
   public static Stage readStage(String path) throws IOException {
     Stage stage = new Stage();
     List<String> lines = Files.readAllLines(Paths.get(path));
-    return new Stage();
+    List<Actor> actors = new ArrayList<Actor>();
+    Grid grid = new Grid();
+
+    //ColRow=Actor
+    for (String cur : lines) {
+      int col = ((int) (cur.charAt(0))) - 65;
+      int row = ((int) cur.charAt(1)) - 48;
+      System.out.println(col + " " + row);
+      // int col = 2;
+      // int row = 2;
+      String name = cur.substring(3);
+      if (name.compareTo("cat") == 0) {
+        actors.add(new Cat(grid.cellAtColRow(col, row).get()));
+      } else if (name.compareTo("dog") == 0) {
+        actors.add(new Dog(grid.cellAtColRow(col, row).get()));
+      } else {
+        actors.add(new Bird(grid.cellAtColRow(col, row).get()));
+      }
+    }
+    
+    stage.actors = actors;
+    stage.grid = grid;
+    return stage;
   }
 }
